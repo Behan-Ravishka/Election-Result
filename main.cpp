8d3a882d4c669aaa22c1f161a5bd30cb8a8753ba
M P Behan Ravishka Perera <165493221+Behan-Ravishka@users.noreply.github.com>
2024-04-26 00:42:50 +0530

    Update main.cpp
    add some comments

--- main.cpp
+++ main.cpp
@@ -6,21 +6,22 @@ using namespace std;
 
 int main()
 {
+    //get variables
     int vote;
     float c1=0,c2=0,c3=0,c4=0,c5=0,i,val_vote=0,rej_vote=0;
     float cp1=0,cp2=0,cp3=0,cp4=0,cp5=0;
-
+    //calculate valid votes
     val_vote=c1+c2+c3+c4+c5;
-
+    //use for loop
     for ( i=1 ; i<=5 ; i++)
     {
         cout << " " << i << ". Enter your vote : ";
         cin >> vote;
-
+    //use if cond. inside the for loop
     if (vote>=1 && vote<=5)
     {
         val_vote+=1;
-
+        //use switch inside the if cod.
         switch (vote)
         {
         case 1:
@@ -44,24 +45,25 @@ int main()
     }
     else
     {
+        //calulate rejected votes
         rej_vote+=1;
     }
 
     }
-
+    //seperate output screens
     cout << "\n----Press any key to see the Election Result..!----" << endl;
     getch ();
-
+    //calculate percentages of votes
     cout.precision(4);
     cp1=(c1*100)/val_vote;
     cp2=(c2*100)/val_vote;
     cp3=(c3*100)/val_vote;
     cp4=(c4*100)/val_vote;
     cp5=(c5*100)/val_vote;
-
+    //beautify
     system("cls");
     system ("color 60");
-
+    //make output 
     cout << "\n-------------------------------------------" << endl;
     cout << "\tBest Project Year -2024" << endl;
     cout << "-------------------------------------------" << endl;
